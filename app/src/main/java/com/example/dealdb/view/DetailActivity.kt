349404f8d40715bc15d6199e9f172f1343f1139d
RicardoTangarife <ricardo.tangarife.97@gmail.com>
2020-04-26 13:43:58 -0500

    Eliminar código innecesario.

--- app/src/main/java/com/example/dealdb/view/DetailActivity.kt
+++ app/src/main/java/com/example/dealdb/view/DetailActivity.kt
@@ -27,19 +27,4 @@ class DetailActivity : AppCompatActivity() {
 
         detailViewModel?.setViews(intent?.getSerializableExtra("cupon") as Offer)
     }
-
-    /*private fun updateUI(cupon: Offer){
-        val URL_IMAGE_DEFECT = "https://img.icons8.com/dusk/64/000000/cutting-coupon.png"
-        tv_title.text = cupon.title
-        tv_store2.text = cupon.store
-        tv_end_date.text = cupon.endDate
-        tv_start_date.text = cupon.startDate
-        tv_description.text = cupon.description
-        if(!cupon.imageUrl.isNullOrEmpty()){
-            Picasso.get().load(cupon.imageUrl).into(iv_picture2)
-        }
-        else{
-            Picasso.get().load(URL_IMAGE_DEFECT).into(iv_picture2)
-        }
-    }*/
 }
